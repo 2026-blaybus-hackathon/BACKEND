472780774f9e9086499d463983b420288aae7cea
Chae <oumuamua121@gmail.com>
2026-01-30 02:10:11 +0900

    feat: 누락한 의존성 추가

--- src/main/kotlin/com/blaybus/backend/repository/ObjectStorageRepository.kt
+++ src/main/kotlin/com/blaybus/backend/repository/ObjectStorageRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository
 import org.springframework.web.multipart.MultipartFile
 import java.io.InputStream
 import java.util.Locale
+import java.util.UUID
 
 @Repository
 class ObjectStorageRepository(
