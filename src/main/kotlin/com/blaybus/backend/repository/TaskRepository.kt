e70e68f8e9bd8965c7ac694d30812a00c3c23def
임동준 <127932430+djlim00@users.noreply.github.com>
2026-02-03 23:05:54 +0900

    feat: N+1 문제 회피를 위한 @EntityGraph 추가

--- src/main/kotlin/com/blaybus/backend/repository/TaskRepository.kt
+++ src/main/kotlin/com/blaybus/backend/repository/TaskRepository.kt
@@ -6,6 +6,7 @@ import com.blaybus.backend.exception.CustomException
 import com.blaybus.backend.exception.ErrorCode
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
@@ -14,5 +15,6 @@ fun TaskRepository.getByTaskId(taskId: Long): Task = findById(taskId).orElseThro
 @Repository
 interface TaskRepository : JpaRepository<Task, Long> {
     // 특정 작성자(멘티)가 쓴 Task 목록 페이징 조회
+    @EntityGraph(attributePaths = ["studyImages", "feedback"])
     fun findByDailyPlannerUser(user: User, pageable: Pageable): Page<Task>
 }
