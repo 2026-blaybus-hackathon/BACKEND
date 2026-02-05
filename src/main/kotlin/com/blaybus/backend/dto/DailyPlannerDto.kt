fed11e2b0fddad8c95195709a94c199d60215200
임동준 <127932430+djlim00@users.noreply.github.com>
2026-02-05 19:50:35 +0900

    refactor: 과제 수정 로직 리팩토링

--- src/main/kotlin/com/blaybus/backend/dto/DailyPlannerDto.kt
+++ src/main/kotlin/com/blaybus/backend/dto/DailyPlannerDto.kt
@@ -1,6 +1,7 @@
 package com.blaybus.backend.dto
 
 import com.blaybus.backend.entity.Subject
+import com.blaybus.backend.entity.Task
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
@@ -58,14 +59,23 @@ data class TaskResponse(
     @Schema(description = "할 일 ID")
     val id: Long,
     @Schema(description = "할 일의 내용")
-    val content: String,
+    val content: String?,
     @Schema(description = "할 일의 과목", allowableValues = ["KOREAN, MATH, ENGLISH, OTHERS"])
     val subject: Subject,
-    @Schema(description = "할 일의 우선순위, 만일 멘토가 준 과제라면 null")
-    val priority: Int?,
     @Schema(description = "할 일에 할당된 공부 시간(분 단위)")
-    val studyTime: Int = 0,
-)
+    val studyDurationInMinutes: Int? = 0,
+){
+    companion object {
+        fun from(task: Task): TaskResponse {
+            return TaskResponse(
+                id = task.id,
+                content = task.title,
+                subject = task.subject,
+                studyDurationInMinutes = task.studyDurationInMinutes ?: 0
+            )
+        }
+    }
+}
 
 data class UpdateDailyPlannerResponse(
     @Schema(description = "업데이트가 적용된 할 일 ID")
